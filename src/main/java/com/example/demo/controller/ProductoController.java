868eebae7a73f74dcd274389ba5f8a43daa56a94
noobmaster25 <sosafloresluisangel@gmail.com>
2023-03-01 22:53:00 -0600

    factorizando controlador de productos

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.DTO.ProductoDTO;
 import com.example.demo.DTO.ProductoNuevoDTO;
-import com.example.demo.entities.Producto;
 import com.example.demo.service.ProductoService;
 
 import jakarta.validation.Valid;
